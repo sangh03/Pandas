9d7d0011e77d7944ad503216c27b164a0f063bcd
sangha03 <iugsanghakara@gmail.com>
2026-09-17 22:55:02 +0530

    learn Series and DataFrames

--- index.py
+++ index.py
@@ -1,1 +1,38 @@
-print("i am learning here Pandas ")
+import pandas as pd
+import numpy as np
+
+
+# Learning the series 
+''' labels=['a','b','d','c']
+
+arr=np.array([12,23,34,45])
+
+d={1:10,2:20,3:30}
+print(pd.Series(arr,index=labels))
+
+print(pd.Series(d)) '''
+
+# Learning the dataframe . multiple series 
+
+
+data = [
+     ['John', 'Anna', 'Peter', 'Linda'],
+     [28, 34, 29, 42],
+     ['New York', 'Paris', 'Berlin', 'London'],
+     [65000, 70000, 62000, 85000]
+]
+
+data1={
+    "name":["sanghakara"],
+    "age":[22]
+}
+
+df=pd.DataFrame(data1)
+
+colums= ["Name","Age","City","Salary"]
+
+df1=pd.DataFrame(data,columns=colums)
+
+
+
+print(df1['Age'])
